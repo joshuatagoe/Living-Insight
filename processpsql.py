bf28ea18d2eb5ea428595120d3cb729a108a234b
JUCHY <jnt297@nyu.edu>
2020-06-12 06:17:14 +0000

    fixed errors in processpsql

--- processpsql.py
+++ processpsql.py
@@ -22,18 +22,20 @@ housing_insight = {
 for row in houses.head(n=5).itertuples():
     if row.job_filing_num in housing_insight["house_id"]:
         continue
-    housing_insight["house_id"].append(row)
+    housing_insight["house_id"].append(row.job_filing_num)
     mental_hosps = housing_insight["mental_hosp"]
-    mental_hosps.append([])
+    mental_hosps.append([])    
     curr_row_hosps = mental_hosps[len(mental_hosps)-1]
     
     address = row.house_num + " " + row.street_name
     #get latitude of house
-    latlong = computedistance.getLagLong(address)
+    latlong = computedistance.getLatLong(address)
+    
+
     for row_2 in mental_health.head(n=20).itertuples():
         latlong2 = [row_2.longitude, row_2.latitude ]
-        if computedistance.computedistance(latlong,latlong2) < 1.5:
+        if computedistance.computeDistance(latlong,latlong2) < 1.5:
             
             curr_row_hosps.append(row_2.query_id)
-            
-        
+
+print(housing_insight)
