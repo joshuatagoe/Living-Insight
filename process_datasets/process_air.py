e1582fd1fd300ed3d3c0705ec4603b957556f481
Joshua Tagoe <34180299+JUCHY@users.noreply.github.com>
2020-06-18 01:41:19 -0400

    completed process_air.py file

--- process_datasets/process_air.py
+++ process_datasets/process_air.py
@@ -7,6 +7,7 @@ Created on Sun Jun 14 06:54:46 2020
 
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
+from pyspark.sql.functions import udf, struct
 import computedistance
 
 
@@ -14,16 +15,18 @@ sc = SparkContext("local", "SparkFile App")
 sc.addFile("/home/ubuntu/Housing-Insight/process_datasets/computedistance.py")
 
 
-def handle_building(building,_311_service):
-    latlong = [building.longitude, building.latitude]
-    latlong2 = [_311_service.longitude, _311_service.latitude ]
-    if computedistance.computeDistance(latlong,latlong2) < 1.5:
-        return True
-    else:   
-        return False
 
-def process_air_quality(row):
-    return None
+def handle_building(building,air_quality):
+    if air_quality.geo_type_name == "Borough":
+        if building.borough.lower() == air_quality.geo_entity_name.lower():
+            return True 
+        else:
+            return False
+    if air_quality.geo_type_name == 'UHF42':
+        if building.community_district == air_quality.geo_entity_id:
+            return True
+        else:
+            return False
 
 spark = SparkSession \
     .builder \
@@ -37,6 +40,18 @@ air_quality = spark.read.format("csv") \
     .load("s3a://living-insight-data/Air_Quality.csv")
 
 
-air_quality.write.jdbc("jdbc:postgresql://localhost:5432/living_insight", table="air_quality", properties = { "user" : "postgres", "password" : "postgres" } )
+buildings = spark.read \
+    .format("jdbc") \
+    .option("url","jdbc:postgresql://localhost:5432/living_insight") \
+    .option("dbtable","buildings") \
+    .option("user","postgres") \
+    .option("password", "postgres") \
+    .load()
+    
 
+air_quality_udf = udf(handle_building,BooleanType())
+
+newdataframe = buildings.crossJoin(air_quality).where(air_quality_udf(struct([buildings[x] for x in buildings.columns]), struct([air_quality[x] for x in air_quality.columns]))).select(buildings.house_id,air_quality.indicator_data_id)
+air_quality.write.jdbc("jdbc:postgresql://localhost:5432/living_insight", table="air_quality", properties = { "user" : "postgres", "password" : "postgres" } )
+newdataframe.write.jdbc("jdbc:postgresql://localhost:5432/living_insight", table="building_to_air_quality", properties = { "user" : "postgres", "password" : "postgres" } )
 spark.stop()
