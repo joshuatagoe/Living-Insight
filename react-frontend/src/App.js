f46349f594fdbe6c7082effd4aad3beedae56ebd
Joshua Tagoe <34180299+JUCHY@users.noreply.github.com>
2020-06-19 08:30:53 -0400

    connecting form results to google maps api for placemarks'

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,32 +1,90 @@
 import React from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
-import Query from './Query'
+import { GoogleMap, withScriptjs, withGoogleMap, Marker} from 'react-google-maps';
 
 
 class Map extends React.Component{ 
   constructor(props){
-    this.placemarks = {};
+    super(props)
+    this.state = {
+      vehicle_collission: 0,
+      subway_entrances: 0,
+      health_services: 0,
+      police_misconduct_reports : 0,
+      response : ""
+
+    }
   }
 
+  handleChange(evt){
+    this.setState({
+        [evt.target.name] : evt.target.value
+        
+    })
+ }
+
   handleSubmit(event){
-    url = "http://ec2-52-91-13-65.compute-1.amazonaws.com:9000/buildings?vehicle_collission=${this.state.vehicle_collission}&subway_entrances=${this.state.subway_entrances}&health_services=${this.state.health_services}"
-    fetch(url)
+    fetch(`http://ec2-52-91-13-65.compute-1.amazonaws.com:9000/buildings?vehicle_collission=${this.state.vehicle_collission}&subway_entrances=${this.state.subway_entrances}&health_services=${this.state.health_services}`)
         .then( res=> res.json())
         .then(res=> this.setState({response: res}))
         .catch(err=>err);
     event.preventDefault();
 }
   render(){
+    let placemarks;
+    let apiResponse = this.state.response;
+    if(apiResponse.length>0){
+      placemarks = apiResponse.map((building)=>(
+        <Marker key={building.house_id} position={{lat:building.latitude, lng: building.longitude}}/>
+        ))
+    }
     return <GoogleMap 
     defaultZoom = {11} 
     defaultCenter = {{ lat : 40.7128, lng: -74.0060 }} 
     >
-      {this.placemarks.map((building)=>(
-      <Marker key={building.house_id} position={{lat:building.latitude, lng: building.longitude}}/>
-      ))}
+      {placemarks}
     </GoogleMap>
 
   }
+
+}
+
+class Query extends React.Component{
+  constructor(props){
+      super(props)
+      this.state = { 
+          collission_query_type : 'precise',
+          entrances_query_type : 'precise',
+          health_services_query_type : 'precise',
+          police_misconduct_query_type : 'precise'
+   }
+  }
+
+  
+  render(){
+      return(
+          <div>
+              <form onSubmit={Map.handleSubmit}>
+                  <label>Vehicle Collissions
+                      <input name="vehicle_collission" type="number" value={this.state.vehicle_collission} onChange={Map.handleChange}/>
+                  </label>
+                  <label>Subway Entrances
+                      <input type="number" name="subway_entrances" value={this.state.subway_entrances} onChange={Map.handleChange}/>
+                  </label>
+                  <label>Mental Health Services
+                      <input type="number" name="health_services" value={this.state.health_services} onChange={Map.handleChange}/>
+                  </label>
+{/*                     <label>Number of Police Misconduct Reports
+                      <input type="number" name="police_misconduct_reports" value={this.state.police_misconduct_reports} onChange={this.props.handleChange}/>
+                  </label> */}
+              <input type="submit" value="Submit"/>
+              </form>
+              <div>{this.state.response}</div>
+
+          </div>
+
+      )
+  }
+
 
 }
 
--- react-frontend/src/Query.js
+++ react-frontend/src/Query.js
@@ -7,16 +7,8 @@ class Query extends React.Component{
             collission_query_type : 'precise',
             entrances_query_type : 'precise',
             health_services_query_type : 'precise',
-            police_misconduct_query_type : 'precise',
-            vehicle_collission: 0,
-            subway_entrances: 0,
-            health_services: 0,
-            police_misconduct_reports : 0,
-            response : ""
-
+            police_misconduct_query_type : 'precise'
      }
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     handleChange(evt){
@@ -26,30 +18,23 @@ class Query extends React.Component{
         })
     }
 
-    handleSubmit(event){
-        url = "http://ec2-52-91-13-65.compute-1.amazonaws.com:9000/buildings?vehicle_collission=${this.state.vehicle_collission}&subway_entrances=${this.state.subway_entrances}&health_services=${this.state.health_services}"
-        fetch(url)
-            .then( res=> res.text())
-            .then(res=> this.setState({response: res}))
-            .catch(err=>err);
-        event.preventDefault();
-    }
+
     
     render(){
         return(
             <div>
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.props.handleSubmit}>
                     <label>Vehicle Collissions
-                        <input name="vehicle_collission" type="number" value={this.state.vehicle_collission} onChange={this.handleChange}/>
+                        <input name="vehicle_collission" type="number" value={this.state.vehicle_collission} onChange={this.props.handleChange}/>
                     </label>
                     <label>Subway Entrances
-                        <input type="number" name="subway_entrances" value={this.state.subway_entrances} onChange={this.handleChange}/>
+                        <input type="number" name="subway_entrances" value={this.state.subway_entrances} onChange={this.props.handleChange}/>
                     </label>
                     <label>Mental Health Services
-                        <input type="number" name="health_services" value={this.state.health_services} onChange={this.handleChange}/>
+                        <input type="number" name="health_services" value={this.state.health_services} onChange={this.props.handleChange}/>
                     </label>
 {/*                     <label>Number of Police Misconduct Reports
-                        <input type="number" name="police_misconduct_reports" value={this.state.police_misconduct_reports} onChange={this.handleChange}/>
+                        <input type="number" name="police_misconduct_reports" value={this.state.police_misconduct_reports} onChange={this.props.handleChange}/>
                     </label> */}
                 <input type="submit" value="Submit"/>
                 </form>
